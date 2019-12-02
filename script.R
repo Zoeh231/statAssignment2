75ca2aef9d4b761fa7a676257a5afc4b144565ee
Zoeh231 <43895759+Zoeh231@users.noreply.github.com>
2019-12-02 01:06:00 +0000

    FINAL

--- script.R
+++ script.R
@@ -34,27 +34,27 @@ impressions <- read_tsv("impressions.tsv")
 
 #creating a function to change all of the timezones in 'clicks' to UTC timezone.#
 convert_timezone <- function(data){  #using seconds as time values, ie. 8 hours = 28800 seconds#
-  if(timezone == 'Pacific time')
+  if(timezone == 'Pacific time') #if the timezone is in pst then we add 8 hours to bring it to utc timezone#
   {
-    if(time >= 68400 )
+    if(time >= 86400 ) # if the hours move over 24 then we add another day to the date#
     {
       date = date + 1
     }
     time == time + 28800
     timezone == 'UTC'
   }
-  elif(timezone =='Eastern time')
+  elif(timezone =='Eastern time') #if timezone is in est then we add 5 hours to bring it to utc timezone
   {
-    if(time >= 68400)
+    if(time >= 86400) # if the hours move over 24 then we add another day to the date#
     {
       date = date + 1
     }
     time == time + 18000
     timezone == 'UTC'
   }
-  else
+  else #this is here as there is already dates and times in the UTC timezone#
   {
-    timezone =='UTC'
+    timezone =='UTC' 
   }
 }
 #Calling funtion on datasets to change timezones and creating new datasets#
